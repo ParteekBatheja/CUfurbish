df71ca26b7974d3712c6e6b5dcc745632a7531f8
Parteek Batheja <121745286+ParteekBatheja@users.noreply.github.com>
2024-04-26 10:22:51 +0530

    Add files via upload

---
+++ order.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buyButton = document.getElementById("buyButton");
+    const orderModal = document.getElementById("orderModal");
+    const closeButton = document.getElementById("closeButton");
+
+    buyButton.addEventListener("click", function () {
+        orderModal.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", function () {
+        orderModal.style.display = "none";
+    });
+});
+
+function goBack() {
+    window.history.back();
+}
